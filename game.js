17ed45f46902a4146ac5beff70d24ae8b72dfba4
jclync <jocelynchen1126@yahoo.com>
2023-03-08 10:09:33 -0800

    deploy game

---
+++ game.js
@@ -0,0 +1,147 @@
+let cookieChar, ground, blobs;
+let spriteX, spriteY, spriteSize, spriteSpeed;
+let song;
+
+let currentScore, highScore, lives, life, gameOver; 
+let noLives = "Lives: 💔 💔 💔";
+let oneLife = "Lives: ❤️‍🔥 💔 💔";
+let twoLives = "Lives: ❤️‍🔥 ❤️‍🔥 💔";
+let threeLives = "Lives: ❤️‍🔥 ❤️‍🔥 ❤️‍🔥";
+
+let messageSent = false;
+
+function setup() {
+  createCanvas(800, 450);
+  spriteSize = 40;
+  spriteX = 60;
+  spriteY = height/2 - spriteSize/2;
+  ground = new Sprite(0, height/4*3, width*3, 5, 'static');
+  blobs = new Group();
+  cookieChar = new Sprite(spriteX, spriteY, spriteSize);
+  world.gravity.y = 10.8;
+  
+  currentScore = 0;
+  highScore = 0;
+  lives = 3;
+  gameOver = false;
+  player = true;
+  spriteSpeed = 0;
+}
+
+function draw() {
+  if (!gameOver) {
+    playGame();
+  }
+}
+
+function playGame() {  
+  //set game screen text
+  setGameScreen();
+  
+  if (random(1) < 0.01) {
+    new blobs.Sprite(width, (height/4*3) - spriteSize/2, spriteSize);
+  }
+  
+  spriteSpeed = 2 + sqrt(currentScore)/5;
+  blobs.move(width*2, 'left', spriteSpeed);
+  
+  if (cookieChar.collides(blobs)) {
+    blobs.removeAll();
+    cookieChar.sleeping = true;
+    updateLives();
+    cookieChar.sleeping = true;
+  }
+}
+
+function setGameScreen() {
+  textSize(15);
+  background(240);
+  fill(0);
+  textAlign(LEFT);
+  currentScore += 1/50;
+  checkLives();
+  text(life + '\nScore: ' + round(currentScore), 30, 30);
+  textAlign(RIGHT);
+  text('High Score: ' + highScore, width - 30, 30); 
+  text('Press space or up arrow to jump. \n Press (q) to quit.', width - 30, (height / 4 * 3) + 50);
+}
+
+// 'space' or uparrow to jump; enter or 'r' to restart
+function keyPressed() {
+  if (keyCode == UP_ARROW) {
+    cookieChar.move(125, 'up', 6.5);
+  } else if (key == ' ') { 
+    cookieChar.move(125, 'up', 6.5);
+  } else if (keyCode == '13') {
+    restart();
+    playGame();
+  } else if (keyCode == '82') {
+    restart();
+    playGame();
+  } else if (keyCode == '81') {
+    endGame();
+  }
+  return false;
+}
+
+function checkLives() {
+  if (lives == 3) {
+    life = threeLives;
+  } else if (lives == 2) {
+    life = twoLives;
+  } else if (lives == 1) {
+    life = oneLife;
+  } else {
+    life = noLives;
+  }
+}
+
+function updateLives() {
+  lives--;
+  console.log(lives);
+  
+  if (lives == 0) {
+    console.log("GAME OVER");
+    endGame();
+  }
+}
+
+function endGame() {
+  gameOver = true;
+  
+  // reset and clear screen
+  cookieChar.sleeping = true;
+  blobs.removeAll();
+  background(0);
+  
+  checkScore();
+  
+  textAlign(CENTER);
+  textSize(40);
+  fill('red');
+  text("Game Over 😔", width/2, height/5);
+  textSize(20);
+  fill(255);
+  text("Your Score: " + round(currentScore) + "\n Your High Score: " + round(highScore) + "\n \n \n Press enter to play again!", width/2, height/3
+      );
+  
+  if (!messageSent) {
+    window.sendMessage(highScore);
+    messageSent = true;
+  }
+  
+  //image(sMole, width/8, height/3*2, 200, 200);
+}
+
+function checkScore() {
+  if (currentScore > highScore) {
+    highScore = round(currentScore);
+  }
+}
+
+function restart() {
+  lives = 3;
+  currentScore = 0;
+  gameOver = false;
+  messageSent = false;
+}
---
+++ index.js
@@ -0,0 +1,179 @@
+import { initializeApp } from "firebase/app";
+import {
+  getAuth,
+  signInWithRedirect,
+  signInAnonymously,
+  onAuthStateChanged,
+  GoogleAuthProvider,
+  signOut,
+} from "firebase/auth";
+import {
+  getFirestore,
+  collection,
+  addDoc,
+  getDocs,
+  onSnapshot,
+  query,
+  orderBy,
+} from "firebase/firestore";
+import { html, render } from "lit-html";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyDWGB2zlJ6yvwDbNgICkWfRZZ2GiF3G6c0",
+    authDomain: "cookie-run-prototype.firebaseapp.com",
+    projectId: "cookie-run-prototype",
+    storageBucket: "cookie-run-prototype.appspot.com",
+    messagingSenderId: "213018018250",
+    appId: "1:213018018250:web:c6a0309beb8841ae354f33"
+  };
+
+let messages = [];
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+// Initialize Auth
+const auth = getAuth(app);
+
+// Initialize Firestore
+const db = getFirestore(app);
+const messagesRef = collection(db, "messages");
+
+// Setup Google authentication
+const provider = new GoogleAuthProvider();
+
+// This function is called if the Sign In button is clicked
+function signInUser() {
+  signInWithRedirect(auth, provider);
+}
+
+function signInAnon() {
+  signInAnonymously(auth)
+    .then(() => {
+      // Signed in..
+    })
+    .catch((error) => {
+      console.error(`Error ${error.code}: ${error.message}.`);
+    });
+}
+
+// This function is called if the Sign Out button is clicked
+function signOutUser() {
+  signOut(auth)
+    .then(() => {
+      console.info("Sign out was successful");
+    })
+    .catch((error) => {
+      console.error(`Error ${error.code}: ${error.message}.`);
+    });
+}
+
+// This function returns a template with the sign in view - what the user sees when they're signed out
+function signInView() {
+  return html`<button class="sign-in" @click=${signInUser}>
+      Sign in with Google
+    </button>
+    <button class="sign-in" @click=${signInAnon}>Anonymous Sign in</button>`;
+}
+
+// This function returns a template with normal app view - what the user sees when they're signed in
+function view() {
+  let user = auth.currentUser;
+  return html`
+    <div id="top-bar">
+      <span>chit chat</span>
+      <span
+        >Signed in as
+        ${user.isAnonymous ? "anon" : auth.currentUser.displayName}</span
+      >
+      <button @click=${signOutUser}>Sign Out</button>
+    </div>
+    <div id="messages-container">
+      ${messages.map(
+        (msg) => html`<div
+          class="message ${msg.uid == user.uid ? "right" : "left"}">
+          <div class="message-content">${msg.content}</div>
+          <div class="message-info">${msg.displayName}</div>
+        </div>`
+      )}
+    </div>
+    <div id="message-composer">
+      <input
+        id="message-entry"
+        @keydown=${type}
+        type="text"
+        placeholder="type here..." />
+    </div>
+  `;
+}
+
+// This is an observer which runs whenever the authentication state is changed
+onAuthStateChanged(auth, (user) => {
+  if (user) {
+    console.log("AUTH STATE CHANGED");
+    const uid = user.uid;
+    console.log(user);
+    // If there is an authenticated user, we render the normal view
+    render(view(), document.body);
+    getAllMessages();
+  } else {
+    // Otherwise, we render the sign in view
+    render(signInView(), document.body);
+  }
+});
+
+/*
+async function sendMessage(message) {
+  console.log("Sending a message!");
+  const user = auth.currentUser;
+  // Add some data to the users collection
+  try {
+    const docRef = await addDoc(collection(db, "messages"), {
+      displayName: user.isAnonymous ? "anon" : user.displayName,
+      uid: user.uid,
+      time: Date.now(),
+      content: message,
+    });
+    console.log("Document written with ID: ", docRef.id);
+  } catch (e) {
+    console.error("Error adding document: ", e);
+  }
+}
+
+//window.sendMessage = sendMessage
+
+async function getAllMessages() {
+  messages = [];
+
+  const querySnapshot = await getDocs(
+    query(messagesRef, orderBy("time", "desc"))
+  );
+  querySnapshot.forEach((doc) => {
+    let msgData = doc.data();
+    messages.push(msgData);
+  });
+  render(view(), document.body);
+}
+
+onSnapshot(
+  collection(db, "messages"),
+  (snapshot) => {
+    console.log("snap", snapshot);
+    getAllMessages();
+  },
+  (error) => {
+    console.error(error);
+  }
+);
+
+function type(e) {
+  if (e.key == "Enter") {
+    sendMessage(e.target.value);
+    e.target.value = "";
+  }
+}
+*/
+
+// string of who you are logged in as
+// let playerId;
+// 
